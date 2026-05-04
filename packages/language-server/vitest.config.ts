17082aa9d4db9a396ea6806d7d25e2cc076d7318
Jimmy Cai <jimmy@cai.im>
2026-05-04 20:16:46 +0200

    Remove vite-tsconfig-paths and `install` which was added by mistake

--- packages/language-server/vitest.config.ts
+++ packages/language-server/vitest.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     test: {
         include: ["tests/**/*.test.ts"],
         exclude: ["dist/**", "node_modules/**"],
     },
-    plugins: [
-        tsconfigPaths()
-    ],
+    resolve: {
+        tsconfigPaths: true
+    }
 });
