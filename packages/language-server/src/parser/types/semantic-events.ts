1afd40b374ddea7b1c47be850778794622f3394d
Jimmy Cai <jimmy@cai.im>
2026-05-11 14:37:51 +0200

    Refactor SemanticEvent types to use type aliases and improve structure

--- packages/language-server/src/parser/types/semantic-events.ts
+++ packages/language-server/src/parser/types/semantic-events.ts
@@ -10,11 +10,28 @@ export type SemanticEvent =
 
 export type ScopeKind = "function" | "flowr";
 
-export interface SemanticScopeEvent {
+export type SemanticScopeEvent = {
     type: "enterScope" | "exitScope";
     range: Range;
     scopeKind: ScopeKind;
-}
+};
+
+export type SemanticEnterDeclarationEvent = {
+    type: "enterDeclaration";
+    declaration: SemanticDeclaration;
+};
+
+export type SemanticExitDeclarationEvent = {
+    type: "exitDeclaration";
+    declaration: SemanticDeclaration;
+};
+
+export type SemanticReferenceEvent = {
+    type: "reference";
+    name: string;
+    kind: "variable" | "function";
+    range: Range;
+};
 
 interface SemanticDeclarationBase<K extends SemanticDeclarationKind> {
     name: string;
@@ -32,21 +49,4 @@ export type SemanticSimpleDeclaration = SemanticDeclarationBase<SemanticSimpleDe
 
 export interface SemanticNamespaceDeclaration extends SemanticDeclarationBase<"namespace"> {
     namespaceUri: string;
-}
-
-export interface SemanticEnterDeclarationEvent {
-    type: "enterDeclaration";
-    declaration: SemanticDeclaration;
-}
-
-export interface SemanticExitDeclarationEvent {
-    type: "exitDeclaration";
-    declaration: SemanticDeclaration;
-}
-
-export interface SemanticReferenceEvent {
-    type: "reference";
-    name: string;
-    kind: "variable" | "function";
-    range: Range;
 }
